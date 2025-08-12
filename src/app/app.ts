8c4b16a2e8d620a9f5ec8684768e463f91b803ff
SergioSoria12 <sergio.soria.secadras@gmail.com>
2025-08-12 10:42:40 +0200

    Terminada parte componentes Curso Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NuevoComponente } from "./nuevo-componente/nuevo-componente";
 import { ComponenteEnLinea } from './componente-en-linea/componente-en-linea';
 import { Interpolacion } from "./interpolacion/interpolacion";
+import { Padre } from "./padre/padre";
 
 @Component({
   selector: 'app-root',
-  imports: [Interpolacion],
+  imports: [Padre],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
