104b1ffd89f104af1946653a2c18cbbc581c72f4
SergioSoria12 <sergio.soria.secadras@gmail.com>
2025-08-12 12:14:17 +0200

    Acabado apartado conceptos básicos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NuevoComponente } from "./nuevo-componente/nuevo-componente";
-import { ComponenteEnLinea } from './componente-en-linea/componente-en-linea';
-import { Interpolacion } from "./interpolacion/interpolacion";
+import { Component } from '@angular/core';
 import { Padre } from "./padre/padre";
+import { Hijo } from "./hijo/hijo";
+import { MostrarMensaje } from "./mostrar-mensaje/mostrar-mensaje";
+import { Replicador } from "./replicador/replicador";
+import { Saludar } from "./saludar/saludar";
 
 @Component({
   selector: 'app-root',
-  imports: [Padre],
+  imports: [Saludar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  titulo = "Bienvenidos";
+  titulo = "Bienvenidos Curso Angular";
 }
---
+++ src/app/hijo/hijo.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  imports: [],
+  templateUrl: './hijo.html',
+  styleUrl: './hijo.css'
+})
+export class Hijo {
+  private titulo = 'Titulo Componente Hijo';
+
+  //Getter de TypeScript
+  get mostrarTitulo(){
+    return this.titulo;
+  }
+
+  getTitulo(){
+    return this.titulo;
+  }
+}
