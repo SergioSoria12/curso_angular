fb0e083dc4ce9f2134d3b76c0f6e990744b14fdb
SergioSoria12 <sergio.soria.secadras@gmail.com>
2025-08-12 13:50:47 +0200

    Terminado apartado flujo de datos y directivas de Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,13 @@ import { Hijo } from "./hijo/hijo";
 import { MostrarMensaje } from "./mostrar-mensaje/mostrar-mensaje";
 import { Replicador } from "./replicador/replicador";
 import { Saludar } from "./saludar/saludar";
+import { ComponenteIf } from "./componente-if/componente-if";
+import { AgregarTarea } from "./agregar-tarea/agregar-tarea";
+import { ComponenteFor } from "./componente-for/componente-for";
 
 @Component({
   selector: 'app-root',
-  imports: [Saludar],
+  imports: [AgregarTarea, ComponenteFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
