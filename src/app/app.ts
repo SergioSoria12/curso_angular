b58ab53a0688f52b7e5d58fe9b277cfcd11e1b9f
SergioSoria12 <sergio.soria.secadras@gmail.com>
2025-08-26 10:02:38 +0200

    Hacer apartado Pipes en Angular del curso

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { Padre } from "./padre/padre";
 import { MostrarMensaje } from "./mostrar-mensaje/mostrar-mensaje";
 import { Replicador } from "./replicador/replicador";
@@ -9,10 +9,17 @@ import { ComponenteFor } from "./componente-for/componente-for";
 import { ViewChildComponent } from "./view-child/view-child";
 import { Mensaje } from './mensaje';
 import { ListadoUsuarios } from "./listado-usuarios/listado-usuarios";
+import { EjemploPipes } from "./ejemplo-pipes/ejemplo-pipes";
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+// Registramos los datos de localizacion para español
+registerLocaleData(localeEs, 'es');
 
 @Component({
   selector: 'app-root',
-  imports: [Padre, ListadoUsuarios],
+  imports: [Padre, ListadoUsuarios, EjemploPipes],
+  providers: [{provide: LOCALE_ID, useValue: 'es'}],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
